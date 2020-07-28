86bcbbdab4a4147103694edfc83cee7757377a3e
stevefan <fan.yuq@husky.neu.edu>
2020-07-27 21:43:36 -0400

    update url2

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ const (
 	// PROJECT_ID = "around-xxx"
 	// BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.225.92.224:9200/"
+	ES_URL = "http://35.225.105.69:9200/"
 )
 
 type Location struct {
